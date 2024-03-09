d73ffb607ee22f9a030203cee46587aab61fc3f1
wiwat_@2009A <wayu061614@gmail.com>
2024-03-09 11:35:28 +0700

    create card info

--- pages/adminpage.tsx
+++ pages/adminpage.tsx
@@ -3,6 +3,8 @@ import { useEffect } from "react";
 import Navbar from "@/component/navbar";
 import Chart from "@/component/chart";
 import BasicTable from "@/component/table";
+import { Box } from "@mui/material";
+import MultiActionAreaCard from "@/component/card";
 
 export default function AdminPage() {
   const router = useRouter();
@@ -17,6 +19,7 @@ export default function AdminPage() {
   return (
     <>
       <Navbar />
+      <MultiActionAreaCard />
       <Chart />
       <BasicTable />
     </>
