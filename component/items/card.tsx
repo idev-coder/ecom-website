45ef608e3c4b564cfcd2fba9d2e32bc815d8e020
wiwat_@2009A <wayu061614@gmail.com>
2024-03-16 21:06:48 +0700

    create brand card and product recommended card and use swiper for create card brand slide

--- component/items/card.tsx
+++ component/items/card.tsx
@@ -8,6 +8,7 @@ import {
   useMediaQuery,
   Box,
 } from "@mui/material";
+import StarIcon from "@mui/icons-material/Star";
 import Router from "next/router";
 
 interface CardProductInfo {
@@ -16,6 +17,28 @@ interface CardProductInfo {
   name: string;
   description: string;
   price: number;
+}
+
+interface CardBrand {
+  id: number;
+  imgPath: string;
+  name: string;
+  description: string;
+}
+
+interface CardCategory {
+  id: number;
+  imgPath: string;
+  category: string;
+}
+
+interface CardProductRecommend {
+  id: number;
+  imgPath: string;
+  name: string;
+  description: string;
+  price: number;
+  star: number;
 }
 
 export const CardProductInfo: React.FC<CardProductInfo> = ({
@@ -44,7 +67,7 @@ export const CardProductInfo: React.FC<CardProductInfo> = ({
     Router.push(`/product/${id}`);
   };
   return (
-    <div className="cardProductInfo" onClick={LinkProductHandle}>
+    <div className="cardProductInfo" onClick={LinkProductHandle} key={id}>
       <Card
         sx={{
           maxWidth: 345,
@@ -81,12 +104,11 @@ export const CardProductInfo: React.FC<CardProductInfo> = ({
   );
 };
 
-export const CardBrand: React.FC<CardProductInfo> = ({
+export const CardBrand: React.FC<CardBrand> = ({
   id,
   imgPath,
   name,
   description,
-  price,
 }) => {
   const isSmallScreen = useMediaQuery("(max-width:490px)");
   const isMediumScreen = useMediaQuery("(max-width:820px)");
@@ -108,10 +130,10 @@ export const CardBrand: React.FC<CardProductInfo> = ({
   };
 
   return (
-    <div className="cardProductInfo" onClick={LinkProductHandle}>
+    <div className="cardProductInfo" onClick={LinkProductHandle} key={id}>
       <Card
         sx={{
-          maxWidth: 345,
+          maxWidth: 340,
           height: ProductCardHeight(),
           position: "relative",
         }}
@@ -160,6 +182,115 @@ export const CardBrand: React.FC<CardProductInfo> = ({
             </Typography>
           </CardContent>
         </CardActionArea>
+      </Card>
+    </div>
+  );
+};
+
+export const CardCategory: React.FC<CardCategory> = ({
+  id,
+  imgPath,
+  category,
+}) => {
+  const LinkProductHandle = () => {
+    Router.push(`/category/${category}`);
+  };
+  return (
+    <div className="cardProductInfo" onClick={LinkProductHandle} key={id}>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image={imgPath}
+            alt={category}
+          />
+          <CardContent>
+            <Typography
+              variant="body2"
+              color={"black"}
+              sx={{ textAlign: "center" }}
+            >
+              {category}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </div>
+  );
+};
+
+export const CardProductRecommend: React.FC<CardProductRecommend> = ({
+  id,
+  imgPath,
+  name,
+  description,
+  price,
+  star,
+}) => {
+  const isSmallScreen = useMediaQuery("(max-width:490px)");
+  const isMediumScreen = useMediaQuery("(max-width:820px)");
+
+  const ProductImgHeight = () => {
+    if (isSmallScreen) return 100;
+    if (isMediumScreen) return 140;
+    return 160;
+  };
+
+  const ProductCardHeight = () => {
+    if (isSmallScreen) return 290;
+    if (isMediumScreen) return 340;
+    return 340;
+  };
+
+  const LinkProductHandle = () => {
+    Router.push(`/product/${id}`);
+  };
+
+  // Example loop to render multiple components
+  const renderStars = () => {
+    const starsArray = [];
+    for (let i = 0; i < star; i++) {
+      starsArray.push(
+        <StarIcon sx={{ color: "#FFC700", fontSize: 15 }} key={i} />
+      );
+    }
+    return starsArray;
+  };
+
+  return (
+    <div className="cardProductInfo" onClick={LinkProductHandle} key={id}>
+      <Card
+        sx={{
+          maxWidth: 345,
+          height: ProductCardHeight(),
+          position: "relative",
+        }}
+      >
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height={ProductImgHeight()}
+            image={imgPath}
+            alt={name}
+            sx={{ objectFit: "contain" }}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h6" component="div">
+              {name}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {description}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <Box sx={{ my: 1, position: "absolute", bottom: 5, left: 20 }}>
+          <Typography variant="body1" color="maroon">
+            {price} Bath
+          </Typography>
+          {/* Render stars using the loop */}
+          {renderStars()}
+        </Box>
       </Card>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,21 @@ import React from "react";
 import Navbar from "@/component/navbar";
 import SwipeCarousel from "@/component/carousel";
 import { Box, Container, Typography, useMediaQuery } from "@mui/material";
-import { CardProductInfo, CardBrand } from "@/component/items/card";
+import {
+  CardProductInfo,
+  CardBrand,
+  CardCategory,
+  CardProductRecommend,
+} from "@/component/items/card";
+import { Navigation, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
-interface Product {
+interface CardProductInfo {
   id: number;
   imgPath: string;
   name: string;
@@ -12,29 +24,136 @@ interface Product {
   price: number;
 }
 
+interface ProductCategory {
+  id: number;
+  imgPath: string;
+  category: string;
+}
+
 const Home: React.FC = () => {
-  const isExtraSmallScreen = useMediaQuery("(max-width:320px)");
+  const isExtraSmallScreen = useMediaQuery("(max-width:360px)");
   const isSmallScreen = useMediaQuery("(max-width:490px)");
   const isMediumScreen = useMediaQuery("(max-width:820px)");
 
   const GridTemplateColumns = () => {
-    if (isExtraSmallScreen) return "1fr 1fr";
+    if (isExtraSmallScreen) return "1fr 1fr ";
     if (isSmallScreen) return "1fr 1fr 1fr";
     if (isMediumScreen) return "1fr 1fr 1fr ";
     return "1fr 1fr 1fr 1fr 1fr 1fr";
   };
 
+  const GridTemplateColumnsCardCategory = () => {
+    const isExtraExtraSmallScreen = useMediaQuery("(max-width:370px)");
+    if (isExtraExtraSmallScreen) return "1fr 1fr";
+    if (isMediumScreen) return "1fr 1fr 1fr ";
+    return "1fr 1fr 1fr 1fr 1fr 1fr";
+  };
+
   const limitCardProduct = () => {
-    if (isExtraSmallScreen) return 4;
     return 6;
   };
 
-  const limitCardBrand = () => {
-    if (isMediumScreen) return 3;
-    return 6;
+  const limitCardCategory = () => {
+    if (isMediumScreen) return 6;
+    return 12;
   };
 
-  const productData: Product[] = [
+  const productData: CardProductInfo[] = [
+    {
+      id: 1,
+      imgPath: "images/1.jpg",
+      name: "Cactus",
+      description:
+        "This is a most beautiful cactus from Sahara desert, the hottest desert in the world.",
+      price: 100,
+    },
+    {
+      id: 1,
+      imgPath: "images/7.jpg",
+      name: "Bag",
+      description:
+        "This is a most beautiful bag and useful from camel leather Sahara desert, the hottest desert in the world.",
+      price: 100,
+    },
+    {
+      id: 1,
+      imgPath: "images/5.jpg",
+      name: "Watch",
+      description: "This is a most beautiful watch, Come with new technology!",
+      price: 100,
+    },
+    {
+      id: 1,
+      imgPath: "images/3.jpg",
+      name: "Cactus",
+      description:
+        "This is a most beautiful cactus from Sahara desert, the hottest desert in the world.",
+      price: 100,
+    },
+    {
+      id: 1,
+      imgPath: "images/4.jpg",
+      name: "Cactus",
+      description:
+        "This is a most beautiful cactus from Sahara desert, the hottest desert in the world.",
+      price: 100,
+    },
+    {
+      id: 1,
+      imgPath: "images/2.jpg",
+      name: "Cactus",
+      description:
+        "This is a most beautiful cactus from Sahara desert, the hottest desert in the world.",
+      price: 100,
+    },
+    {
+      id: 1,
+      imgPath: "images/1.jpg",
+      name: "Cactus",
+      description:
+        "This is a most beautiful cactus from Sahara desert, the hottest desert in the world.",
+      price: 100,
+    },
+    {
+      id: 1,
+      imgPath: "images/1.jpg",
+      name: "Cactus",
+      description:
+        "This is a most beautiful cactus from Sahara desert, the hottest desert in the world.",
+      price: 100,
+    },
+    {
+      id: 1,
+      imgPath: "images/1.jpg",
+      name: "Cactus",
+      description:
+        "This is a most beautiful cactus from Sahara desert, the hottest desert in the world.",
+      price: 100,
+    },
+    {
+      id: 1,
+      imgPath: "images/1.jpg",
+      name: "Cactus",
+      description:
+        "This is a most beautiful cactus from Sahara desert, the hottest desert in the world.",
+      price: 100,
+    },
+    {
+      id: 1,
+      imgPath: "images/1.jpg",
+      name: "Cactus",
+      description:
+        "This is a most beautiful cactus from Sahara desert, the hottest desert in the world.",
+      price: 100,
+    },
+    {
+      id: 1,
+      imgPath: "images/1.jpg",
+      name: "Cactus",
+      description:
+        "This is a most beautiful cactus from Sahara desert, the hottest desert in the world.",
+      price: 100,
+    },
     {
       id: 1,
       imgPath: "images/1.jpg",
@@ -84,6 +203,21 @@ const Home: React.FC = () => {
     },
   ];
 
+  const ProductCategory: ProductCategory[] = [
+    { id: 1, imgPath: "/images/1.jpg", category: "SmartPhone" },
+    { id: 1, imgPath: "/images/1.jpg", category: "SmartPhone" },
+    { id: 1, imgPath: "/images/1.jpg", category: "SmartPhone" },
+    { id: 1, imgPath: "/images/1.jpg", category: "SmartPhone" },
+    { id: 1, imgPath: "/images/1.jpg", category: "SmartPhone" },
+    { id: 1, imgPath: "/images/1.jpg", category: "SmartPhone" },
+    { id: 1, imgPath: "/images/1.jpg", category: "SmartPhone" },
+    { id: 1, imgPath: "/images/1.jpg", category: "SmartPhone" },
+    { id: 1, imgPath: "/images/1.jpg", category: "SmartPhone" },
+    { id: 1, imgPath: "/images/1.jpg", category: "SmartPhone" },
+    { id: 1, imgPath: "/images/1.jpg", category: "SmartPhone" },
+    { id: 1, imgPath: "/images/1.jpg", category: "SmartPhone" },
+  ];
+
   return (
     <>
       <Navbar />
@@ -93,40 +227,89 @@ const Home: React.FC = () => {
           display: "grid",
           gridTemplateColumns: GridTemplateColumns(),
           gap: 2,
-          my: 5,
+          my: 4,
         }}
       >
-        {productData.slice(0, limitCardProduct()).map((product: Product) => (
-          <CardProductInfo
-            key={product.id}
-            id={product.id}
-            imgPath={product.imgPath}
-            name={product.name}
-            description={product.description}
-            price={product.price}
-          />
-        ))}
-      </Container>
-      <Container>
-        <Box>
-          <Typography variant="h5">E-Mall</Typography>
-        </Box>
-        <Box
-          sx={{
-            display: "grid",
-            gridTemplateColumns: GridTemplateColumns(),
-            gap: 2,
-            my: 5,
-          }}
-        >
-          {productData.slice(0, limitCardBrand()).map((product: Product) => (
-            <CardBrand
+        {productData
+          .slice(0, limitCardProduct())
+          .map((product: CardProductInfo) => (
+            <CardProductInfo
               key={product.id}
               id={product.id}
               imgPath={product.imgPath}
               name={product.name}
               description={product.description}
               price={product.price}
+            />
+          ))}
+      </Container>
+      <Container>
+        <Box>
+          <Typography variant="h5">E-Mall</Typography>
+        </Box>
+        <Swiper
+          slidesPerView={6}
+          autoplay={{
+            delay: 3500,
+            disableOnInteraction: false,
+          }}
+          modules={[Autoplay, Pagination, Navigation]}
+          className="mySwiper"
+        >
+          {productData.map((product: CardProductInfo) => (
+            <SwiperSlide key={product.id}>
+              <CardBrand
+                key={product.id}
+                id={product.id}
+                name={product.name}
+                imgPath={product.imgPath}
+                description={product.description}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </Container>
+      <Container sx={{ mb: 4 }}>
+        <Box>
+          <Typography variant="h5">Categories</Typography>
+        </Box>
+        <Box
+          sx={{
+            display: "grid",
+            gridTemplateColumns: GridTemplateColumnsCardCategory(),
+          }}
+        >
+          {ProductCategory.slice(0, limitCardCategory()).map(
+            (category: ProductCategory) => (
+              <CardCategory
+                id={category.id}
+                imgPath={category.imgPath}
+                category={category.category}
+              />
+            )
+          )}
+        </Box>
+      </Container>
+      <Container>
+        <Box>
+          <Typography variant="h5">Recommended</Typography>
+        </Box>
+        <Box
+          sx={{
+            display: "grid",
+            gridTemplateColumns: GridTemplateColumns(),
+            gap: 2,
+          }}
+        >
+          {productData.map((product: CardProductInfo) => (
+            <CardProductRecommend
+              key={product.id}
+              id={product.id}
+              imgPath={product.imgPath}
+              name={product.name}
+              description={product.description}
+              price={product.price}
+              star={4}
             />
           ))}
         </Box>
